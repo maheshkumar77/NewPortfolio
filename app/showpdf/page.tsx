fe3e8c728999977e83c75aee0646aa19df5d72fa
maheshkumar77 <149643073+maheshkumar77@users.noreply.github.com>
2025-01-21 02:05:45 +0530

    Add files via upload

---
+++ app/showpdf/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <iframe src="/MaheshKumarSahuResume (13).pdf" width="100%" height="600px"></iframe>
+    </div>
+  )
+}
+
+export default page
